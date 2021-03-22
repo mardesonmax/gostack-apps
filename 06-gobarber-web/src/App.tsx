0d036c2a904d92b4f3ccaa34e6890da8ef0cf9bd
Mardeson Pereira <maxpb777@gmail.com>
2021-03-22 13:14:44 -0300

    Iniciando projeto GoBarber

---
+++ 06-gobarber-web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello Word</h1>
+    <GlobalStyles />
+  </>
+);
+
+export default App;
