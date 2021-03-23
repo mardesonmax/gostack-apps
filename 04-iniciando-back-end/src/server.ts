85e3ca953fb0e6ebc896c8a59fe0e77423a9d219
Mardeson Pereira <maxpb777@gmail.com>
2021-03-23 15:32:06 -0300

    cors

--- 04-iniciando-back-end/src/server.ts
+++ 04-iniciando-back-end/src/server.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -10,6 +12,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
